szwjp/homeproxy

commit f387ede7730bce47e9ec685b30c0618718385dc4

feat(init): append chnlist to chinadns-ng
Closes: #77

root/etc/homeproxy/scripts/update_resources.sh
@@ -96,6 +96,7 @@ case "$1" in
 	;;
 "china_list")
 	check_list_update "$1" "Loyalsoldier/v2ray-rules-dat" "release" "direct-list.txt"
+	sed -i -e "s/full://g" -e "/:/d" "$RESOURCES_DIR/china_list.txt"
 	;;
 *)
 	echo -e "Usage: $0 <china_ip4 / china_ip6 / gfw_list / china_list>"

root/etc/init.d/homeproxy
@@ -94,7 +94,7 @@ start_service() {
 			fi
 			;;
 		"proxy_mainland_china")
-			sed -r -e "s/full://g" -e "/:/d" -e "s/(.*)/server=\/\1\/127.0.0.1#$dns_port/g" \
+			sed -r -e "s/(.*)/server=\/\1\/127.0.0.1#$dns_port/g" \
 				"$HP_DIR/resources/china_list.txt" > "$DNSMASQ_DIR/china_list.conf"
 			;;
 		"custom"|"global")
@@ -192,11 +192,13 @@ start_service() {
 			procd_append_param command --trust-dns "127.0.0.1#$dns_port"
 			procd_append_param command --ipset-name4 "inet@fw4@homeproxy_mainland_addr_v4"
 			procd_append_param command --ipset-name6 "inet@fw4@homeproxy_mainland_addr_v6"
+			procd_append_param command --chnlist-file "$HP_DIR/resources/china_list.txt"
 			procd_append_param command --gfwlist-file "$HP_DIR/resources/gfw_list.txt"
 			[ "$ipv6_support" -eq "1" ] || procd_append_param command --no-ipv6=tC
 
 			if [ -x "/sbin/ujail" ]; then
 				procd_add_jail "chinadns-ng" log
+				procd_add_jail_mount "$HP_DIR/resources/china_list.txt"
 				procd_add_jail_mount "$HP_DIR/resources/gfw_list.txt"
 				procd_set_param capabilities "/etc/capabilities/homeproxy.json"
 				procd_set_param no_new_privs 1

commit 016e18041568080c83e56fa2f9623c051a862514

fix(scripts/resources): fix override return code
Fixes: #99
Fixes: f387ede7730b ("feat(init): append chnlist to chinadns-ng")
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

root/etc/homeproxy/scripts/update_resources.sh
@@ -95,8 +95,8 @@ case "$1" in
 	check_list_update "$1" "Loyalsoldier/v2ray-rules-dat" "release" "gfw.txt"
 	;;
 "china_list")
-	check_list_update "$1" "Loyalsoldier/v2ray-rules-dat" "release" "direct-list.txt"
-	sed -i -e "s/full://g" -e "/:/d" "$RESOURCES_DIR/china_list.txt"
+	check_list_update "$1" "Loyalsoldier/v2ray-rules-dat" "release" "direct-list.txt" && \
+		sed -i -e "s/full://g" -e "/:/d" "$RESOURCES_DIR/china_list.txt"
 	;;
 *)
 	echo -e "Usage: $0 <china_ip4 / china_ip6 / gfw_list / china_list>"