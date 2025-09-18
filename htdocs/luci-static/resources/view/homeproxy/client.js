szwjp/homeproxy

commit e568efd3e06e1c70610024bce02b09dc47006726

fix(client): fix set dns outbound
DNS outbound now defaults to `direct` and no need to specify it
manually.

Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -1028,7 +1028,7 @@ return view.extend({
 			delete this.keylist;
 			delete this.vallist;
 
-			this.value('direct-out', _('Direct'));
+			this.value('', _('Direct'));
 			uci.sections(data[0], 'routing_node', (res) => {
 				if (res.enabled === '1')
 					this.value(res['.name'], res.label);
@@ -1036,8 +1036,6 @@ return view.extend({
 
 			return this.super('load', section_id);
 		}
-		so.default = 'direct-out';
-		so.rmempty = false;
 		so.editable = true;
 		/* DNS servers end */
 

root/etc/homeproxy/scripts/generate_client.uc
@@ -421,13 +421,11 @@ config.dns = {
 		{
 			tag: 'default-dns',
 			type: 'udp',
-			server: wan_dns,
-			detour: 'direct-out'
+			server: wan_dns
 		},
 		{
 			tag: 'system-dns',
-			type: 'local',
-			detour: 'direct-out'
+			type: 'local'
 		}
 	],
 	rules: [],
@@ -473,7 +471,6 @@ if (!isEmpty(main_node)) {
 				server: 'default-dns',
 				strategy: 'prefer_ipv6'
 			},
-			detour: 'direct-out',
 			...parse_dnserver(china_dns_server)
 		});
 

root/etc/homeproxy/scripts/migrate_config.uc
@@ -65,10 +65,10 @@ if (!migration_crontab) {
 }
 
 /* log_level was introduced */
-if (isEmpty(uci.get(uciconfig, ucimain, 'log_level'))
+if (isEmpty(uci.get(uciconfig, ucimain, 'log_level')))
 	uci.set(uciconfig, ucimain, 'log_level', 'warn');
 
-if (isEmpty(uci.get(uciconfig, uciserver, 'log_level'))
+if (isEmpty(uci.get(uciconfig, uciserver, 'log_level')))
 	uci.set(uciconfig, uciserver, 'log_level', 'warn');
 
 /* empty value defaults to all ports now */
@@ -148,6 +148,10 @@ uci.foreach(uciconfig, ucidnsserver, (cfg) => {
 		dns_server_migration[cfg['.name']].client_subnet = cfg.client_subnet;
 		uci.delete(uciconfig, cfg['.name'], 'client_subnet');
 	}
+
+	/* dns outbound defaults to direct in sb 1.12 */
+	if (cfg.outbound === 'direct-out')
+		uci.delete(uciconfig, cfg['.name'], 'outbound');
 });
 
 /* DNS rules options */

commit 789065262a48b7738eb1be52806bcc06ddbab6d0

fixup(client): fix set dns outbound
Fixes: e568efd3e06e ("fix(client): fix set dns outbound")
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -1028,7 +1028,7 @@ return view.extend({
 			delete this.keylist;
 			delete this.vallist;
 
-			this.value('', _('Direct'));
+			this.value('direct-out', _('Direct'));
 			uci.sections(data[0], 'routing_node', (res) => {
 				if (res.enabled === '1')
 					this.value(res['.name'], res.label);
@@ -1036,6 +1036,8 @@ return view.extend({
 
 			return this.super('load', section_id);
 		}
+		so.default = 'direct-out';
+		so.rmempty = false;
 		so.editable = true;
 		/* DNS servers end */
 

root/etc/homeproxy/scripts/generate_client.uc
@@ -421,11 +421,13 @@ config.dns = {
 		{
 			tag: 'default-dns',
 			type: 'udp',
-			server: wan_dns
+			server: wan_dns,
+			detour: self_mark ? 'direct-out' : null
 		},
 		{
 			tag: 'system-dns',
-			type: 'local'
+			type: 'local',
+			detour: self_mark ? 'direct-out' : null
 		}
 	],
 	rules: [],
@@ -471,6 +473,7 @@ if (!isEmpty(main_node)) {
 				server: 'default-dns',
 				strategy: 'prefer_ipv6'
 			},
+			detour: self_mark ? 'direct-out' : null,
 			...parse_dnserver(china_dns_server)
 		});
 
@@ -510,6 +513,10 @@ if (!isEmpty(main_node)) {
 		if (cfg.enabled !== '1')
 			return;
 
+		let outboud = get_outbound(cfg.outbound);
+		if (outbound === 'direct-out' && isEmpty(self_mark))
+			outbound = null;
+
 		push(config.dns.servers, {
 			tag: 'cfg-' + cfg['.name'] + '-dns',
 			type: cfg.type,
@@ -525,7 +532,7 @@ if (!isEmpty(main_node)) {
 				server: get_resolver(cfg.address_resolver || dns_default_server),
 				strategy: cfg.address_strategy
 			} : null,
-			detour: get_outbound(cfg.outbound)
+			detour: outbound
 		});
 	});
 

root/etc/homeproxy/scripts/migrate_config.uc
@@ -148,10 +148,6 @@ uci.foreach(uciconfig, ucidnsserver, (cfg) => {
 		dns_server_migration[cfg['.name']].client_subnet = cfg.client_subnet;
 		uci.delete(uciconfig, cfg['.name'], 'client_subnet');
 	}
-
-	/* dns outbound defaults to direct in sb 1.12 */
-	if (cfg.outbound === 'direct-out')
-		uci.delete(uciconfig, cfg['.name'], 'outbound');
 });
 
 /* DNS rules options */